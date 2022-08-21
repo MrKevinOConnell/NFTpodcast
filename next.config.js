66b8ea9323aacee17127f4220b00a595b5586872
Kevin O'Connell <kevinoconnell42@gmail.com>
2022-08-21 16:39:41 -0400

    change back lets see

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-        ],
-      },
-    ];
-  },
-};
+  swcMinify: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
